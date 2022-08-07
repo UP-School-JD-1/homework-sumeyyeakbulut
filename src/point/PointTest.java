26d8aecfd7e1931e09ff0a4bb287a5d32fc77167
sumeyyeakbulut <sumeyyeakb@gmail.com>
2022-08-07 23:36:04 +0300

    first commit

---
+++ src/point/PointTest.java
@@ -0,0 +1,22 @@
+package point;
+
+public class PointTest {
+    public static void main(String[] args) {
+
+        Point point = new Point(3, 4);
+        Point point2 = new Point();
+        Point point3 = new Point(1, 1);
+
+        System.out.println(point.distanceToOrigin());
+        System.out.println(point2.distanceToOrigin());
+
+        System.out.println(point.distanceBetweenTwoPoint(0,0));
+        System.out.println(point.distanceBetweenTwoPoint(point3));
+
+        System.out.println(point.getX());
+        System.out.println(point.getY());
+        point.move(6, -6);
+        System.out.println(point.getX());
+        System.out.println(point.getY());
+    }
+}
